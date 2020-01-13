6ff4db205d983e9f5b1032b1dc0dde84bc929997
hoffs <stephanie.nhoffman7@gmail.com>
2020-01-12 21:56:37 -0800

    added NewsArticle.js, Notes.js, and server.js code; created views folder and handlebars files

--- server.js
+++ server.js
@@ -31,3 +31,58 @@ mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true 
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
   });
+
+  // === Routes ===
+
+  app.get("/", function(req, res) {
+      res.render("index");
+  })
+
+  // === A GET route for scrpaing the website === 
+  app.get("/scrape", function(req, res) {
+      // === First we grab the body of the html with axios ===
+      axios.get("https://www.seattletimes.com/").then(function(response) {
+          // === Then we load that into cheerio and save it to $ for a shorthand selector ===
+          var $ = cheerio.load(response.data);
+            // === Now we grab every h2 tag within an article tag === 
+          $("article h2").each(function(i, element) {
+            // === Save the h2 info in an empty result object === 
+            var result = {};
+
+            // === Add text and href of everylink, and save them as properties of the result object ===
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            // === Create a new NewsArticles using the 'result' object built from scraping ===
+            db.NewsArticle.create(result)
+                .then(function(NewsArticle) {
+                    // === Viewing the newly added result === 
+                    console.log(NewsArticle);
+                })
+                .catch(function(err) {
+                    // === Logging an error if occured ===
+                    console.log(err);
+                });
+          });
+          // === Sending a message the to client the scrape is complete ===
+          res.send("Scrape has been Completed");
+      });
+  });
+
+  // === Route for getting all NewsArticles from the db ===
+  app.get("/NewsArticle", function(req, res) {
+      // === Grabbing every document in the NewsArticle collection
+      db.NewsArticle.find({})
+        .then(function(dbNewsArticle) {
+            // === If we were successful in finding NewsArticle, send them back to client ===
+            res.json(dbNewsArticle);
+        })
+        .catch(function(err) {
+            // === If an error occurred, send it to the client ===
+            res.json(err);
+        });
+  });
